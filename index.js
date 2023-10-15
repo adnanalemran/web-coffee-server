1de5b1fe0d1fdff8b5de958a27d9d53760924881
adnanalemran <adnanalemranontor@gmail.com>
2023-10-16 01:16:58 +0600

    v-01

--- index.js
+++ index.js
@@ -9,7 +9,9 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.swu9d.mongodb.net/?retryWrites=true&w=majority`;
+ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.fhwdeyh.mongodb.net/?retryWrites=true&w=majority`;
+
+ 
 console.log(uri);
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
